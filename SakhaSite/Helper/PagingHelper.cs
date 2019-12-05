76c36e966cd957445c020ea069f40ce02c6885e7
ZahraSakha <58354187+ZahraSakha@users.noreply.github.com>
2019-12-05 12:23:42 +0330

    add  SakhaSite/Helper/

---
+++ SakhaSite/Helper/PagingHelper.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SakhaSite.Models.ViewModels;
+
+namespace SakhaSite.Helpers
+{
+    public class PagingHelper
+    {
+        public IList<PagingItem> GetPagings(int current, int count)
+        {
+            const int itemsCount = 10;
+            var results = new List<PagingItem>();
+            if (current < 1) current = 1;
+            if (current > count) current = count;
+
+            if (count <= itemsCount)
+            {
+                for (int i = 1; i <= count; i++)
+                {
+                    results.Add(new PagingItem()
+                    {
+                        Display = i.ToString(),
+                        PageNumber = i,
+                        Active = current == i,
+                    });
+                }
+
+                return results;
+            }
+            if (current < itemsCount / 2)
+            {
+                for (int i = 1; i <= itemsCount - 2; i++)
+                {
+                    results.Add(new PagingItem()
+                    {
+                        Display = i.ToString(),
+                        PageNumber = i,
+                        Active = current == i,
+                    });
+                }
+                results.Add(new PagingItem()
+                {
+                    Display = "...",
+                    PageNumber = (count - current) / 2,
+                    Active = false,
+                });
+                results.Add(new PagingItem()
+                {
+                    Display = count.ToString(),
+                    PageNumber = count,
+                    Active = false,
+                });
+                return results;
+            }
+            if (current > count - itemsCount / 2)
+            {
+                results.Add(new PagingItem()
+                {
+                    Display = 1.ToString(),
+                    PageNumber = 1,
+                    Active = false,
+                });
+                results.Add(new PagingItem()
+                {
+                    Display = "...",
+                    PageNumber = current / 2,
+                    Active = false,
+                });
+
+                for (int i = count - (itemsCount - 3); i <= count; i++)
+                {
+                    results.Add(new PagingItem()
+                    {
+                        Display = i.ToString(),
+                        PageNumber = i,
+                        Active = current == i,
+                    });
+                }
+
+                return results;
+            }
+
+            results.Add(new PagingItem()
+            {
+                Display = 1.ToString(),
+                PageNumber = 1,
+                Active = false,
+            });
+            results.Add(new PagingItem()
+            {
+                Display = "...",
+                PageNumber = current / 2,
+                Active = false,
+            });
+            for (int i = current - (itemsCount / 2) + 2; i <= current + (itemsCount / 2) - 2; i++)
+            {
+                results.Add(new PagingItem()
+                {
+                    Display = i.ToString(),
+                    PageNumber = i,
+                    Active = current == i,
+                });
+            }
+            results.Add(new PagingItem()
+            {
+                Display = "...",
+                PageNumber = current + (count - current) / 2,
+                Active = false,
+            });
+            results.Add(new PagingItem()
+            {
+                Display = count.ToString(),
+                PageNumber = count,
+                Active = false,
+            });
+            return results;
+        }
+    }
+}
