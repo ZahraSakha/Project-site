f95f31d06b7a8ee5fcafd6275b3fc24b9e4d871b
ZahraSakha <58354187+ZahraSakha@users.noreply.github.com>
2019-12-05 12:21:16 +0330

    add  SakhaSite/Contexts/

---
+++ SakhaSite/Contexts/AuthDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SakhaSite.Models.DataModels;
+
+namespace SakhaSite.Contexts
+{
+    public class AuthDbContext : IdentityDbContext<AppUser>
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ SakhaSite/Contexts/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SakhaSite.Models.DataModels;
+using SakhaSite.Models.DataModels.Configuration;
+
+namespace SakhaSite.Contexts
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CategoryConfiguration).Assembly);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductFeaturesConfiguration).Assembly);
+        }
+    }
+}
