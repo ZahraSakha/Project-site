36ec59c327291a85dd3ede9d2e69fbe76691f47e
ZahraSakha <58354187+ZahraSakha@users.noreply.github.com>
2019-12-05 12:20:25 +0330

    add SakhaSite/Constants/

---
+++ SakhaSite/Constants/ClaimConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SakhaSite.Constants
+{
+    public class ClaimConstants
+    {
+        public const string FullName = "FullName";
+    }
+}
---
+++ SakhaSite/Constants/RoleConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SakhaSite.Constants
+{
+    public class RoleConstants
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
