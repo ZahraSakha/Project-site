ff3e27504cfc9ca1e2f5abcc309c1a5ff8b43220
ZahraSakha <58354187+ZahraSakha@users.noreply.github.com>
2019-12-05 12:23:09 +0330

    add SakhaSite/Extensions/

---
+++ SakhaSite/Extensions/IApplicationBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SakhaSite.Extensions
+{
+    public static class IApplicationBuilderExtensions
+    {
+        public static void MigrateDatabase<TContext>(this IApplicationBuilder app) where TContext : DbContext
+        {
+            using (var scope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                using (var context = scope.ServiceProvider.GetService<TContext>())
+                {
+                    context.Database.Migrate();
+                }
+            }
+        }
+    }
+}
