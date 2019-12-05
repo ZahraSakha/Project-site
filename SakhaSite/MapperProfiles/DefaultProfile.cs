98b404d3fb3700dabc0b9b005766b79016cd01c2
ZahraSakha <58354187+ZahraSakha@users.noreply.github.com>
2019-12-05 12:24:23 +0330

    add  SakhaSite/MapperProfiles/

---
+++ SakhaSite/MapperProfiles/DefaultProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SakhaSite.Models.DataModels;
+using SakhaSite.Models.ViewModels;
+
+namespace SakhaSite.MapperProfiles
+{
+    public class DefaultProfile : Profile
+    {
+        public DefaultProfile()
+        {
+            CreateMap<Product, ProductViewModel>();
+        }
+    }
+}
